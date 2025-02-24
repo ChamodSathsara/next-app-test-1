929697cc39caa538c5fdc6eabcbd5570eaf64555
ChamodSathsara <sathsarawapc@gmail.com>
2025-02-24 14:00:50 +0530

    not Found component done

---
+++ src/app/not-found.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <h1 className="text-red-700 font-bold text-4xl">this page Not Fount</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident
+        repellat veniam illum? Quisquam maiores qui, laudantium excepturi
+        sapiente recusandae officia optio provident magni.
+      </p>
+    </div>
+  );
+}
+
+export default NotFound;
